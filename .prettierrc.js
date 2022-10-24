b87fc6a18387f4f53e6ab148fbe25d088d10cf26
Zoran Jambor <info@zoranjambor.com>
2022-10-25 00:42:29 +0200

    Add initial Prettier config
    Prettier’s playground was used to generate this config, with the idea of extending it later on.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,20 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-    tabWidth: 6,
+        arrowParens: "always",
+        bracketSameLine: false,
+        bracketSpacing: true,
+        embeddedLanguageFormatting: "off",
+        htmlWhitespaceSensitivity: "ignore",
+        insertPragma: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: "preserve",
+        quoteProps: "as-needed",
+        requirePragma: false,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 4,
+        trailingComma: "es5",
+        useTabs: true,
+        vueIndentScriptAndStyle: false
   };
