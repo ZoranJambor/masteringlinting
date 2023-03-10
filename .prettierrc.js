c16324f1b80c09ca69c712570a60ffa19b538faa
Zoran Jambor <info@zoranjambor.com>
2023-03-10 17:37:41 +0100

    Add configuration overrides config
    For the demo, we’ve changed the “singleAttributePerLine” option just for HTML files, which means that VUE and JSX files will be formatted with the default value specified in config (true).
    
    In contrast, HTML files will be formatted with the value specified in the overrides object (false).

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,21 +1,29 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-        arrowParens: "always",
-        bracketSameLine: false,
-        bracketSpacing: true,
-        embeddedLanguageFormatting: "off",
-        htmlWhitespaceSensitivity: "ignore",
-        insertPragma: false,
-        jsxSingleQuote: false,
-        printWidth: 120,
-        proseWrap: "preserve",
-        quoteProps: "as-needed",
-        requirePragma: false,
-        semi: false,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: "es5",
-        useTabs: true,
-        vueIndentScriptAndStyle: false,
-        singleAttributePerLine: true
-  };
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'off',
+	htmlWhitespaceSensitivity: 'ignore',
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 120,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+	vueIndentScriptAndStyle: false,
+	singleAttributePerLine: true,
+	overrides: [
+		{
+			files: '*.html',
+			options: {
+				singleAttributePerLine: false,
+			},
+		}
+	],
+}
