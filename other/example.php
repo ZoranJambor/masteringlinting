25fb7c07ccfc3f898cac26ad980ad08957ebbee0
Zoran Jambor <info@zoranjambor.com>
2023-03-28 23:48:58 +0200

    Add PHP & Tailwind plugins for Prettier
    To demonstrate how Prettier plugins work, an official plugin for PHP and a third-party plugin for TailwindCSS were installed using NPM.

--- other/example.php
+++ other/example.php
@@ -1,17 +1,24 @@
 <?php
-array_map(
-function ($arg1, $arg2) use ($var1, $var2) {
-    return $arg1 + $arg2 / ($var + $var2);     },
-    [ 'complex' => 'code',  'with' => function () { return 'inconsistent'; },
-        'formatting' => 'is',
-                'hard' => 'to',
-              'maintain' => true,
-    ]
-);
+/**
+ * @format
+ */
 
-?>
+array_map(
+	function ($arg1, $arg2) use ($var1, $var2) {
+		return $arg1 + $arg2 / ($var + $var2);
+	},
+	[
+		'complex' => 'code',
+		'with' => function () {
+			return 'inconsistent';
+		},
+		'formatting' => 'is',
+		'hard' => 'to',
+		'maintain' => true,
+	]
+); ?>
 <article class="test"   >
 
     My article.
-        <h1 class="headline">  A headline—<?php echo "OH NO!"; ?></h1>
+        <h1 class="headline">  A headline—<?php echo 'OH NO!'; ?></h1>
 </article>
